c84a221686128598b326876f29168246e31a481f
Matt <matt@kyros.cloud>
2021-04-20 16:52:56 +0000

    Changes for latest dataset run

--- helmScanner/runner.py
+++ helmScanner/runner.py
@@ -9,6 +9,7 @@ import tarfile
 import pygraphviz as pgv
 import glob
 import re
+import logging
 
 from helmScanner.collect import artifactHubCrawler
 from helmScanner.output import result_writer
@@ -18,7 +19,7 @@ from helmScanner.output import result_writer
 # Local setup of checkov
 from checkov.helm.registry import registry
 from checkov.helm.runner import Runner as helm_runner
-from checkov.kubernetes.runner import Runner as k8_runner
+#from checkov.kubernetes.runner import Runner as k8_runner
 
 
 RESULTS_PATH = f'{os.path.abspath(os.path.curdir)}/results'
@@ -31,7 +32,7 @@ globalDepsList = defaultdict(list)
 emptylist = []
 
 def extract(tar_url, extract_path='.'):
-    print(tar_url)
+    logging.debug(tar_url)
     tar = tarfile.open(tar_url, 'r')
     for item in tar:
         tar.extract(item, extract_path)
@@ -57,7 +58,7 @@ def parse_helm_dependency_output(o):
 def scan_files():
     crawler = artifactHubCrawler.ArtifactHubCrawler()
     crawlDict, totalRepos, totalPackages = crawler.mockCrawl()
-    print(f"Crawl completed with {totalPackages} charts from {totalRepos} repositories.")
+    logging.info(f"Crawl completed with {totalPackages} charts from {totalRepos} repositories.")
 
     checks_table = []
     summary_table = []
@@ -68,7 +69,8 @@ def scan_files():
     download_failures = []
     parse_deps_failures = []
 
-    chartNameFromResultDataExpression = '.*.RELEASE-NAME-(.*).default'
+    chartNameFromResultDataExpression = '(.*)\.(RELEASE-NAME-)?(.*)(\.default)?'
+    chartNameFromResultDataExpressionGroup = 3
 
     for repoCount in crawlDict:
         repo = crawlDict[repoCount]
@@ -84,7 +86,7 @@ def scan_files():
             #if orgRepoFilename == "reponame":
             #    continue
             if True:
-                print(f"Scanning {repo['repoName']}/{chartPackage['name']}| Download Source ")
+                logging.info(f"Scanning {repo['repoName']}/{chartPackage['name']}| Download Source ")
                 # Setup local dir and download
                 repoChartPathName = f"{repo['repoName']}/{chartPackage['name']}"
                 downloadPath = f'{RESULTS_PATH}/{repoChartPathName}'
@@ -96,35 +98,33 @@ def scan_files():
                     for filename in glob.glob(f"{downloadPath}/**.tgz", recursive=False):
                         try: 
                             extract(filename, downloadPath)
-                            print(f"Scanning {repo['repoName']}/{chartPackage['name']}| Extract Source ")
+                            logging.info(f"Scanning {repo['repoName']}/{chartPackage['name']}| Extract Source ")
                             os.remove(filename)
                         except:
-                            print(f"Failed to extract {repo['repoName']}/{chartPackage['name']}")
+                            logging.info(f"Failed to extract {repo['repoName']}/{chartPackage['name']}")
                             extract_failures.append([f"{repo['repoName']}/{chartPackage['name']}"])
                             continue
                 except:
-                    print(f"Failed to download {repo['repoName']}/{chartPackage['name']}")
+                    logging.info(f"Failed to download {repo['repoName']}/{chartPackage['name']}")
                     download_failures.append([f"{repo['repoName']}/{chartPackage['name']}"])
                     continue
 
-                runner = helm_runner()
-
-                print(f"SCAN OF {repo['repoName']}/{chartPackage['name']} | Processing Chart Deps")
+                logging.info(f"SCAN OF {repo['repoName']}/{chartPackage['name']} | Processing Chart Deps")
                 proc = subprocess.Popen(["helm", 'dependency', 'list' , f"{downloadPath}/{chartPackage['name']}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 o, e = proc.communicate()
                 if e:
                     if "Warning: Dependencies" in str(e, 'utf-8'):
-                        print(f"V1 API chart without Chart.yaml dependancies. Skipping chart dependancy list for {chartPackage['name']} at dir: {downloadPath}/{chartPackage['name']}. Error details: {str(e, 'utf-8')}")
+                        logging.info(f"V1 API chart without Chart.yaml dependancies. Skipping chart dependancy list for {chartPackage['name']} at dir: {downloadPath}/{chartPackage['name']}. Error details: {str(e, 'utf-8')}")
                     else: 
-                        print(f"Error processing helm dependancies for {chartPackage['name']} at source dir: {downloadPath}/{chartPackage['name']}. Error details: {str(e, 'utf-8')}")
+                        logging.info(f"Error processing helm dependancies for {chartPackage['name']} at source dir: {downloadPath}/{chartPackage['name']}. Error details: {str(e, 'utf-8')}")
                 chart_deps = parse_helm_dependency_output(o)
-                print(chart_deps)
+                logging.info(chart_deps)
 
     #### GRAPH. INITIAL NODE AND DEPS   
                 if chart_deps:
                     depGraph.add_node(repoChartPathName, label=repoChartPathName, color='green', shape='circle') # adds current repo to graph - ONLY if there are deps.
                     for key in chart_deps:
-                        print(f" GRAPHING DEPS FOR {chart_deps[key]}")
+                        logging.info(f" GRAPHING DEPS FOR {chart_deps[key]}")
                         current_dep = chart_deps[key]
                         depCombinedRepoName = f'{list(current_dep.values())[2]}/{list(current_dep.values())[0]}' #Consistent repo/chart naming for our dep
                         depRepoNameLabel = f'{list(current_dep.values())[0]}'
@@ -133,12 +133,12 @@ def scan_files():
                         globalDepsList[depCombinedRepoName].append(repoChartPathName)
                         nodecolor = "black"
                         if repo['repoName'] in list(current_dep.values())[2]:
-                            print(f"{depCombinedRepoName} (repo name in dep url): is probably a local repo dep for {repoChartPathName} Coloring blue")
+                            logging.info(f"{depCombinedRepoName} (repo name in dep url): is probably a local repo dep for {repoChartPathName} Coloring blue")
                             nodecolor = "blue"
                         elif "http" not in list(current_dep.values())[2]:
-                             print(f"{depCombinedRepoName} (no url, local files): is probably a local repo dep for {repoChartPathName} leaving black")
+                             logging.info(f"{depCombinedRepoName} (no url, local files): is probably a local repo dep for {repoChartPathName} leaving black")
                         else:
-                            print(f"{depCombinedRepoName} is probably a remote repo dep for {repoChartPathName} Coloring red")
+                            logging.info(f"{depCombinedRepoName} is probably a remote repo dep for {repoChartPathName} Coloring red")
                             nodecolor = "red"
                         depGraph.add_node(depCombinedRepoName, label=depRepoNameLabel, shape='circle', color=nodecolor) #create dep repo if non exist.
                         depGraph.add_edge(repoChartPathName, depCombinedRepoName) # Link our repo to the dep repo, will only link to the same source (new node if helm repo is from a different source)
@@ -153,24 +153,27 @@ def scan_files():
                           list(current_dep.values())[3]  #dep dict chart_status
                      ] """
                 
+                # Assign results_scan outside of try objects.
                 results_scan = object
                 try:
-                    print(f"SCAN OF {repo['repoName']}/{chartPackage['name']} | Running Checkov")
+                    logging.info(f"SCAN OF {repo['repoName']}/{chartPackage['name']} | Running Checkov")
+                    runner = helm_runner()
                     results_scan = runner.run(root_folder=downloadPath, external_checks_dir=None, files=None)
                     res = results_scan.get_dict()
-                    print(f"SCAN OF {repo['repoName']}/{chartPackage['name']} | Processing Results")
+                    logging.info(f"SCAN OF {repo['repoName']}/{chartPackage['name']} | Processing Results")
                     for passed_check in res["results"]["passed_checks"]:
-                        chartNameFromResultData = re.search(chartNameFromResultDataExpression, passed_check["resource"]).group(1)
+                        chartNameFromResultData = re.search(chartNameFromResultDataExpression, passed_check["resource"]).group(chartNameFromResultDataExpressionGroup)
+                        ## NEW. Default items if no key exists for non-critical components
                         check = [
                             repoChartPathName,
                             repo['repoName'],
                             chartPackage['name'],
                             chartPackage['version'],
                             chartPackage['ts'],
-                            chartPackage['signed'],
-                            chartPackage['security_report_created_at'],
+                            chartPackage.get('signed','no data'),
+                            chartPackage.get('security_report_created_at','no data'),
                             chartNameFromResultData,   
-                            chartPackage['is_operator'],
+                            chartPackage.get('is_operator','no data'),
                             str(check_category(passed_check["check_id"])).lstrip("CheckCategories."),
                             passed_check["check_id"],
                             passed_check["check_name"],
@@ -185,20 +188,45 @@ def scan_files():
                             repo['repoRaw']['official'],
                             repo['repoRaw']['scanner_disabled']
                             ]
-                        #check.extend(add_meta(scan_time))
+
+                        # check = [
+                        #     repoChartPathName,
+                        #     repo['repoName'],
+                        #     chartPackage['name'],
+                        #     chartPackage['version'],
+                        #     chartPackage['ts'],
+                        #     chartPackage['signed'],
+                        #     chartPackage['security_report_created_at'],
+                        #     chartNameFromResultData,   
+                        #     chartPackage['is_operator'],
+                        #     str(check_category(passed_check["check_id"])).lstrip("CheckCategories."),
+                        #     passed_check["check_id"],
+                        #     passed_check["check_name"],
+                        #     passed_check["check_result"]["result"],
+                        #     passed_check["file_path"],
+                        #     passed_check["check_class"],
+                        #     passed_check["resource"].split(".")[0],
+                        #     repo['repoRaw']['repository_id'],
+                        #     repo['repoRaw']['digest'],
+                        #     repo['repoRaw']['last_tracking_ts'],
+                        #     repo['repoRaw']['verified_publisher'],
+                        #     repo['repoRaw']['official'],
+                        #     repo['repoRaw']['scanner_disabled']
+                        #     ]
+    
                         result_lst.append(check)
                     for failed_check in res["results"]["failed_checks"]:
-                        chartNameFromResultData = re.search(chartNameFromResultDataExpression, passed_check["resource"]).group(1)
+                        chartNameFromResultData = re.search(chartNameFromResultDataExpression, failed_check["resource"]).group(chartNameFromResultDataExpressionGroup)
                         check = [
                             repoChartPathName,
                             repo['repoName'],
                             chartPackage['name'],
                             chartPackage['version'],
                             chartPackage['ts'],
-                            chartPackage['signed'],
-                            chartPackage['security_report_created_at'],
+                            chartPackage.get('signed','no data'),
+                            chartPackage.get('security_report_created_at','no data'),
                             chartNameFromResultData,   
-                            chartPackage['is_operator'],
+                            chartPackage.get('is_operator','no data'),
                             str(check_category(failed_check["check_id"])).lstrip("CheckCategories."),
                             failed_check["check_id"],
                             failed_check["check_name"],
@@ -222,10 +250,10 @@ def scan_files():
                             chartPackage['name'],
                             chartPackage['version'],
                             chartPackage['ts'],
-                            chartPackage['signed'],
-                            chartPackage['security_report_created_at'],
-                            "empty scan",  
-                            chartPackage['is_operator'],
+                            chartPackage.get('signed','no data'),
+                            chartPackage.get('security_report_created_at','no data'),
+                            "empty scan",   
+                            chartPackage.get('is_operator','no data'),
                             "empty scan",
                             "empty scan",
                             "empty scan",
@@ -244,7 +272,7 @@ def scan_files():
                         result_lst.append(check)
                         #empty_resources = self.module_resources()
                 except Exception:
-                    print('unexpected error in scan')
+                    logging.info('unexpected error in scan')
                     exc_type, exc_value, exc_traceback = sys.exc_info()
                     tb = traceback.format_exception(exc_type, exc_value, exc_traceback)
                     check = [
@@ -253,10 +281,10 @@ def scan_files():
                             chartPackage['name'],
                             chartPackage['version'],
                             chartPackage['ts'],
-                            chartPackage['signed'],
-                            chartPackage['security_report_created_at'],
+                            chartPackage.get('signed','no data'),
+                            chartPackage.get('security_report_created_at','no data'),
                             "error in scan",   
-                            chartPackage['is_operator'],
+                            chartPackage.get('is_operator','no data'),
                             "error in scan",
                             "error in scan",
                             "error in scan",
@@ -276,36 +304,34 @@ def scan_files():
 
                 # Summary Results
                 try:
-                    print(f"SCAN OF {repo['repoName']}/{chartPackage['name']} | Processing Summaries")
+                    logging.info(f"SCAN OF {repo['repoName']}/{chartPackage['name']} | Processing Summaries")
                     res = results_scan.get_dict()
-                    chartNameFromResultData = re.search(chartNameFromResultDataExpression, passed_check["resource"]).group(1)
                     summary_lst_item = [
                         repoChartPathName,
                         repo['repoName'],
                         chartPackage['name'],
                         chartPackage['version'],
                         chartPackage['ts'],
-                        chartPackage['signed'],
-                        chartPackage['security_report_created_at'],
-                        chartNameFromResultData,   
-                        chartPackage['is_operator'],
+                        chartPackage.get('signed', 'No Data'),
+                        chartPackage.get('security_report_created_at', 'No Data'),
+                        chartPackage['name'],   
+                        chartPackage.get('is_operator', 'No Data'),
                         "success",
                         res["summary"]["passed"],
                         res["summary"]["failed"],
                         res["summary"]["parsing_errors"]
                     ]
                 except:
-                    chartNameFromResultData = re.search(chartNameFromResultDataExpression, passed_check["resource"]).group(1)
                     summary_lst_item = [
                         repoChartPathName,
                         repo['repoName'],
                         chartPackage['name'],
                         chartPackage['version'],
                         chartPackage['ts'],
-                        chartPackage['signed'],
-                        chartPackage['security_report_created_at'],
-                        chartNameFromResultData,   
-                        chartPackage['is_operator'],
+                        chartPackage.get('signed', 'No Data'),
+                        chartPackage.get('security_report_created_at', 'No Data'),
+                        chartPackage['name'],   
+                        chartPackage.get('is_operator', 'No Data'),
                         "failed",
                         0,
                         0,
@@ -316,11 +342,11 @@ def scan_files():
                 # Helm Dependancies
                 try:
                     res = results_scan.get_dict()
-                    print(f"SCAN OF {repo['repoName']}/{chartPackage['name']} | Processing Helm Dependancies")
+                    logging.info(f"SCAN OF {repo['repoName']}/{chartPackage['name']} | Processing Helm Dependancies")
                     #{'common': {'chart_name': 'common', 'chart_version': '0.0.5', 'chart_repo': 'https://charts.adfinis.com', 'chart_status': 'unpacked'}}
                     if chart_deps:
                         for key in chart_deps:
-                            print(f" HELMDEP FOUND! {chart_deps[key]}")
+                            logging.info(f" HELMDEP FOUND! {chart_deps[key]}")
                             current_dep = chart_deps[key]
                             
                             dep_item = [
@@ -336,7 +362,7 @@ def scan_files():
 
                             helmdeps_lst.append(dep_item)
 
-                    print(f"CURRENT HELMDEPS LIST {helmdeps_lst}")
+                    logging.info(f"CURRENT HELMDEPS LIST {helmdeps_lst}")
                         
                 except:
                     pass
@@ -344,8 +370,8 @@ def scan_files():
         # Dep graph per repo
         if helmdeps_lst:
             depGraph.draw(f"results/helm-deps-{repo['repoName']}.png", prog='circo')
-        print(f"Global deps usage: {globalDepsUsage}")
-        print(f"Global deps list {globalDepsList}")
+        logging.info(f"Global deps usage: {globalDepsUsage}")
+        logging.info(f"Global deps list {globalDepsList}")
 
         result_writer.print_csv(summary_lst, result_lst, helmdeps_lst, empty_resources, RESULTS_PATH, repo['repoName'], orgRepoFilename, globalDepsList, globalDepsUsage)
         empty_resources_total.update(empty_resources)
@@ -357,8 +383,8 @@ def check_category(check_id):
 
 def run():
     checks_table, summary_table, all_resources, empty_resources, all_dataobj = scan_files()
-    #result_writer.print_summary_csv(summary_table, checks_table, empty_resources, RESULTS_PATH)
+    #result_writer.info_summary_csv(summary_table, checks_table, empty_resources, RESULTS_PATH)
     
     #if os.environ.get('RESULT_BUCKET'):
-    #    print(f'Uploading results to {os.environ["RESULT_BUCKET"]}')
+    #    logging.info(f'Uploading results to {os.environ["RESULT_BUCKET"]}')
     #    s3_uploader.upload_results(RESULTS_PATH, SCAN_TIME)
