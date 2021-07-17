d6bb9eb04f850c791a510195febe863a618db923
Matt Johnson <matt@bridgecrew.io>
2021-07-17 14:36:24 +0100

    debug info for file uploads

--- helmScanner/output/s3_uploader.py
+++ helmScanner/output/s3_uploader.py
@@ -9,7 +9,9 @@ s3 = boto3.client('s3')
 
 def upload_results_to_s3(results_path, scan_time, partialUpload):
     for filename in os.listdir(results_path):
+        helmscanner_logging.info(f'Found file: {filename}')
         if filename.lower().endswith('.csv'):
+            helmscanner_logging.info(f'Uploading file: {filename}')
             s3.upload_file(f'{results_path}/{filename}', os.environ['RESULT_BUCKET'], f'results/{scan_time}/{filename}')
             helmscanner_logging.info(f'Uploaded {filename}')
             if partialUpload:
