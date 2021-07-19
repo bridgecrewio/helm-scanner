15b23cf47170f21ff2aff594b92b9c4103dbee3d
Matt Johnson <matt@bridgecrew.io>
2021-07-19 10:44:14 +0100

    Sort outputs into directories on upload

--- helmScanner/output/s3_uploader.py
+++ helmScanner/output/s3_uploader.py
@@ -9,17 +9,27 @@ s3 = boto3.client('s3')
 
 def upload_results_to_s3(results_path, scan_time, partialUpload):
     for filename in os.listdir(results_path):
-        helmscanner_logging.info(f'Found file: {filename}')
+        helmscanner_logging.debug(f'Found file: {filename}')
         if filename.lower().endswith('.csv'):
-            helmscanner_logging.info(f'Uploading file: {filename}')
+            if filename.lower().startswith('summarytable-'):
+                uploadSubDir = "summarytables"
+            if filename.lower().startswith('deps-table-'):
+                uploadSubDir = "depstables"
+            if filename.lower().startswith('checks-table-'):
+                uploadSubDir = "checkstables"
+            if filename.lower().endswith('_summary.csv'):
+                uploadSubDir = "imagescan_summaries"
+            else:
+                uploadSubDir = "imagescans"
+            helmscanner_logging.info(f'Uploading file: {filename} to subdir {uploadSubDir}')
             try: 
-              s3.upload_file(f'{results_path}/{filename}', os.environ['RESULT_BUCKET'], f'results/{scan_time}/{filename}')
+              s3.upload_file(f'{results_path}/{filename}', os.environ['RESULT_BUCKET'], f'results/{scan_time}/{uploadSubDir}/{filename}')
               helmscanner_logging.info(f'Uploaded {filename}')
               if partialUpload:
                 helmscanner_logging.info(f'Partial upload selected, renaming {filename} to {filename}.uploaded')
                 os.rename(filename, f"{filename}.uploaded")
             except Exception as e:
-              helmscanner_logging.error(f'Failed to upload via boto3. Error was: {e}')
+              helmscanner_logging.error(f'Failed to upload {filename} via boto3. Error was: {e}')
 
 
 def upload_results(results_path, scan_time, partialUpload):
