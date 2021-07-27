10f8ad3e0435f7213b11acb627f97ec4516bc86b
Matt Johnson <matt@bridgecrew.io>
2021-07-27 15:20:10 +0100

    move CSV directory logic into CSV writer, to remove from uploader

--- helmScanner/output/result_writer.py
+++ helmScanner/output/result_writer.py
@@ -5,14 +5,14 @@ def print_csv(sum_table, chk_table, helmdeps_lst, empty_resources, path, repo, o
     checks_frame = pd.DataFrame(chk_table, columns=[ 'runner timestamp', 'combined name', "repository name", "package name", "package latest version",
                                                     'package created at', 'package is signed', 'security report created timestamp', 'helm chart', 'resource is operator',
                                                     'check catagory', 'check id', 'check name', 'check result', 'file path', "check class", "resource id", "repository id", "repository digest", "repository tracking ts", "repository verified", "repository official", "repository scanning disbled" ])
-    checks_frame.to_csv(f'{path}/checks-table-{orgRepoFilename}.csv')
+    checks_frame.to_csv(f'{path}/checks/checks-table-{orgRepoFilename}.csv')
     
 
     # create summary table:
     summary_frame = pd.DataFrame(sum_table, columns=[ 'runner timestamp', 'combined name', "repository name", "package name", "package latest version",
                                                      'package created at', 'package is signed', 'security report created timestamp', 'helm chart', 'resource is operator', 'scan status', 
                                                      'passed checks', 'failed checks', 'parsing errors' ])
-    summary_frame.to_csv(f'{path}/summarytable-{orgRepoFilename}.csv')
+    summary_frame.to_csv(f'{path}/summaries/summarytable-{orgRepoFilename}.csv')
 
 
     # Chart Deps
@@ -29,7 +29,7 @@ def print_csv(sum_table, chk_table, helmdeps_lst, empty_resources, path, repo, o
 
     chart_deps_frame = pd.DataFrame(helmdeps_lst, columns=[ 'runner timestamp', 'combined name', "repository name", "package name", "package latest version",
                                                      'dep helm chart', 'dep helm version', 'dep repo', 'dep chart status' ])
-    chart_deps_frame.to_csv(f'{path}/deps-table-{orgRepoFilename}.csv')
+    chart_deps_frame.to_csv(f'{path}/deps/deps-table-{orgRepoFilename}.csv')
 
     # Global deps statistics
     global_deps_stats = pd.DataFrame.from_dict(globaldepsusage, orient='index')
