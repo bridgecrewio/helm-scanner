2f59a81afaa911b5f681fc389699dfaa9a2b36a2
Matt Johnson <matt@bridgecrew.io>
2021-07-27 15:27:06 +0100

    Updated uploader to use new folder structure from CSV writer

--- helmScanner/output/s3_uploader.py
+++ helmScanner/output/s3_uploader.py
@@ -8,18 +8,20 @@ s3 = boto3.client('s3')
 
 
 def upload_results_to_s3(results_path, scan_time, partialUpload):
-    for filename in os.listdir(results_path):
-        helmscanner_logging.info(f'Found file: {filename}')
-        if filename.lower().endswith('.csv'):
-            helmscanner_logging.info(f'Uploading file: {filename}')
-            try: 
-              s3.upload_file(f'{results_path}/{filename}', os.environ['RESULT_BUCKET'], f'results/{scan_time}/{filename}')
-              helmscanner_logging.info(f'Uploaded {filename}')
-              if partialUpload:
-                helmscanner_logging.info(f'Partial upload selected, renaming {filename} to {filename}.uploaded')
-                os.rename(filename, f"{filename}.uploaded")
-            except Exception as e:
-              helmscanner_logging.error(f'Failed to upload via boto3. Error was: {e}')
+    subdirList = ['checks','summaries','deps','containers','container_summaries']
+    for subdir in subdirList:
+        for filename in os.listdir(f"{results_path}/{subdir}"):
+            helmscanner_logging.info(f'Found file: {subdir}/{filename}')
+            if filename.lower().endswith('.csv'):
+                helmscanner_logging.info(f'Uploading file: {subdir}/{filename}')
+                try: 
+                    s3.upload_file(f'{results_path}/{subdir}/{filename}', os.environ['RESULT_BUCKET'], f'results/{scan_time}/{subdir}/{filename}')
+                    helmscanner_logging.info(f'Uploaded {subdir}/{filename}')
+                    if partialUpload:
+                        helmscanner_logging.info(f'Partial upload selected, renaming {subdir}/{filename} to {subdir}/{filename}.uploaded')
+                        os.rename(filename, f"{filename}.uploaded")
+                except Exception as e:
+                    helmscanner_logging.error(f'Failed to upload via boto3. Error was: {e}')
 
 
 def upload_results(results_path, scan_time, partialUpload):
