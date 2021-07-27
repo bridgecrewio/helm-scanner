1b787085831e3a1cae927dc586412ec3d8ca4002
Matt Johnson <matt@bridgecrew.io>
2021-07-27 15:37:05 +0100

    capture dockerfiles to s3

--- helmScanner/output/s3_uploader.py
+++ helmScanner/output/s3_uploader.py
@@ -22,6 +22,17 @@ def upload_results_to_s3(results_path, scan_time, partialUpload):
                         os.rename(filename, f"{filename}.uploaded")
                 except Exception as e:
                     helmscanner_logging.error(f'Failed to upload via boto3. Error was: {e}')
+    for filename in os.listdir(f"{results_path}/dockerfiles"):
+        if filename.lower().endswith('.Dockerfile'):
+                helmscanner_logging.info(f'Uploading file: dockerfiles/{filename}')
+                try: 
+                    s3.upload_file(f'{results_path}/dockerfiles/{filename}', os.environ['RESULT_BUCKET'], f'results/{scan_time}/dockerfiles/{filename}')
+                    helmscanner_logging.info(f'Uploaded {subdir}/{filename}')
+                    if partialUpload:
+                        helmscanner_logging.info(f'Partial upload selected, renaming dockerfiles/{filename} to dockerfiles/{filename}.uploaded')
+                        os.rename(filename, f"{filename}.uploaded")
+                except Exception as e:
+                    helmscanner_logging.error(f'Failed to upload via boto3. Error was: {e}')
 
 
 def upload_results(results_path, scan_time, partialUpload):
